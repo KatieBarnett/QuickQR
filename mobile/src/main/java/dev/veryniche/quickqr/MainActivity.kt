81ed0dc9b60704f757abbf052d6fee8af4be9e3e
Katie Barnett <katiebarnett5@gmail.com>
2023-09-25 22:08:29 +1000

    Initial commit + theme

---
+++ mobile/src/main/java/dev/veryniche/quickqr/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.veryniche.quickqr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
