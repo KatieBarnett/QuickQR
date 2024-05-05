d93fdd4b2ca6db7770812759f9a41934f5bee09a
Katie Barnett <katiebarnett5@gmail.com>
2024-05-05 11:42:57 +1000

    Version code update for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 }
 
 ext {
-    extra["appVersionName"] = "2.0.1"
-    extra["appVersionCode"] = 27
+    extra["appVersionName"] = "2.0.2"
+    extra["appVersionCode"] = 28
     extra["compileSdk"] = 34
     extra["targetSdk"] = 34
     extra["minSdk"] = 26
