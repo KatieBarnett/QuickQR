a2dccc16c631eb87916a027622745163ff07368f
Katie Barnett <katiebarnett5@gmail.com>
2024-06-02 18:20:57 +1000

    Added more icons

--- core/src/main/java/dev/veryniche/quickqr/core/model/QRIcon.kt
+++ core/src/main/java/dev/veryniche/quickqr/core/model/QRIcon.kt
@@ -3,33 +3,55 @@ package dev.veryniche.quickqr.core.model
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountBalance
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.BreakfastDining
+import androidx.compose.material.icons.rounded.BrunchDining
+import androidx.compose.material.icons.rounded.BugReport
+import androidx.compose.material.icons.rounded.Coffee
 import androidx.compose.material.icons.rounded.DateRange
+import androidx.compose.material.icons.rounded.DriveEta
+import androidx.compose.material.icons.rounded.EmojiFoodBeverage
 import androidx.compose.material.icons.rounded.Face
+import androidx.compose.material.icons.rounded.Fastfood
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material.icons.rounded.Hearing
 import androidx.compose.material.icons.rounded.Help
 import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.HourglassBottom
 import androidx.compose.material.icons.rounded.Language
+import androidx.compose.material.icons.rounded.Liquor
+import androidx.compose.material.icons.rounded.LocalBar
+import androidx.compose.material.icons.rounded.LocalPizza
 import androidx.compose.material.icons.rounded.Lock
+import androidx.compose.material.icons.rounded.LunchDining
 import androidx.compose.material.icons.rounded.Paid
 import androidx.compose.material.icons.rounded.Print
 import androidx.compose.material.icons.rounded.QrCode2
 import androidx.compose.material.icons.rounded.QrCodeScanner
 import androidx.compose.material.icons.rounded.QuestionAnswer
+import androidx.compose.material.icons.rounded.RamenDining
 import androidx.compose.material.icons.rounded.Route
 import androidx.compose.material.icons.rounded.Savings
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.ShoppingBag
 import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.material.icons.rounded.SmartToy
+import androidx.compose.material.icons.rounded.SportsBar
+import androidx.compose.material.icons.rounded.SportsBaseball
+import androidx.compose.material.icons.rounded.SportsBasketball
+import androidx.compose.material.icons.rounded.SportsCricket
+import androidx.compose.material.icons.rounded.SportsRugby
+import androidx.compose.material.icons.rounded.SportsSoccer
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material.icons.rounded.ThumbUp
 import androidx.compose.material.icons.rounded.TrendingUp
 import androidx.compose.material.icons.rounded.VideoLibrary
 import androidx.compose.material.icons.rounded.ViewInAr
 import androidx.compose.material.icons.rounded.Visibility
+import androidx.compose.material.icons.rounded.Watch
 import androidx.compose.material.icons.rounded.Web
 import androidx.compose.material.icons.rounded.Widgets
+import androidx.compose.material.icons.rounded.WineBar
 import androidx.compose.material.icons.rounded.Work
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -63,7 +85,29 @@ enum class QRIcon(val vector: ImageVector, val showInList: Boolean = true) {
     Work(Icons.Rounded.Work),
     Print(Icons.Rounded.Print),
     AccountBalance(Icons.Rounded.AccountBalance),
-    ViewInAr(Icons.Rounded.ViewInAr)
+    ViewInAr(Icons.Rounded.ViewInAr),
+    HourglassBottom(Icons.Rounded.HourglassBottom),
+    SmartToyBeer(Icons.Rounded.SmartToy),
+    DriveEta(Icons.Rounded.DriveEta),
+    BugReport(Icons.Rounded.BugReport),
+    Watch(Icons.Rounded.Watch),
+    SportsRugby(Icons.Rounded.SportsRugby),
+    SportsBasketball(Icons.Rounded.SportsBasketball),
+    SportsBaseball(Icons.Rounded.SportsBaseball),
+    SportsCricket(Icons.Rounded.SportsCricket),
+    SportsSoccer(Icons.Rounded.SportsSoccer),
+    SportsBar(Icons.Rounded.SportsBar),
+    LocalBar(Icons.Rounded.LocalBar),
+    Coffee(Icons.Rounded.Coffee),
+    Liquor(Icons.Rounded.Liquor),
+    WineBar(Icons.Rounded.WineBar),
+    EmojiFoodBeverage(Icons.Rounded.EmojiFoodBeverage),
+    LocalPizza(Icons.Rounded.LocalPizza),
+    BreakfastDining(Icons.Rounded.BreakfastDining),
+    BrunchDining(Icons.Rounded.BrunchDining),
+    LunchDining(Icons.Rounded.LunchDining),
+    RamenDining(Icons.Rounded.RamenDining),
+    Fastfood(Icons.Rounded.Fastfood),
 }
 
 fun getRandomIcon(): QRIcon {
